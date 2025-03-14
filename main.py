1da6f069dbfc6eb44c02a9b287b5403e83307dc7
Evgenii Sarychev <sarychev98.16@gmail.com>
2025-03-14 21:21:58 +0400

    my comment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from graphic_arts.start_game_banner import run_screensaver
 from random import randint
 
 
@@ -80,16 +81,16 @@ def choice_char_class():
     return char_class
 
 
-def main():
+def main() -> None:
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
-
-main()
+main() 
